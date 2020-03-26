413b9969e647899442a27ec546ad75c6bb00d21e
Naoko <f906710584c@gmail.com>
2020-03-26 18:52:24 +0800

    change

--- sazs34/task/weather_pro2.js
+++ sazs34/task/weather_pro2.js
@@ -6,7 +6,7 @@ let config = {
     darksky_api: "b089f565812416bc9f507d013434c947", //从https://darksky.net/dev/ 上申请key填入即可
     aqicn_api: "df8924acde3f31bc7e47a6b91258aca9c4c46e5f", //从http://aqicn.org/data-platform/token/#/ 上申请key填入即可
     huweather_apiKey: "6f92b94c86384062b930ab1f24d05641", //和风天气APIkey,可自行前往 https://dev.heweather.com/ 进行获取(注意key类型选WebApi)
-    lat_lon: "30.215328, 120.285778", //请填写经纬度,直接从谷歌地图中获取即可
+    lat_lon: "22.722583,114.394868", //请填写经纬度,直接从谷歌地图中获取即可
     lang: "zh", //语言,随意切换为您想要的语言哦(zh/zh-tw/ja/en/fr/...)
     log: 0, //调试日志,0为不开启,1为开启,2为开启精简日志
     timeout: 0, //超时时间,单位毫秒(1000毫秒=1秒),一般不推荐修改[为0则不限制超时时间]
