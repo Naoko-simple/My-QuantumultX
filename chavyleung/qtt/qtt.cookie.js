2523f361cd6b7e038f27f9be15be0d555bb6f372
Naoko-simple <f906710584c@gmail.com>
2020-04-09 22:02:42 +0800

    chavyleung 增加【趣头条】签到

---
+++ chavyleung/qtt/qtt.cookie.js
@@ -0,0 +1,65 @@
+const cookieName = '趣头条'
+const signurlKey = 'senku_signurl_qtt'
+const signheaderKey = 'senku_signheader_qtt'
+const signbodyKey = 'senku_signbody_qtt'
+const senku = init()
+
+const requrl = $request.url
+if ($request && $request.method != 'OPTIONS') {
+  try {
+    const signurlVal = requrl
+    const signheaderVal = JSON.stringify($request.headers)
+
+    if (signurlVal) senku.setdata(signurlVal, signurlKey)
+    if (signheaderVal) senku.setdata(signheaderVal, signheaderKey)
+    senku.msg(cookieName, `获取Cookie: 成功`, ``)
+    senku.log(`🔔${signurlVal},🔔${signheaderVal}`)
+  } catch (error) {
+    senku.log(`❌error:${error}`)
+  }
+}
+
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+senku.done()
